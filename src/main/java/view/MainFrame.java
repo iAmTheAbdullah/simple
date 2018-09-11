2426db44ab273ae24f9fdad3b5a17603095b71a6
Abdullah Malik <asmalik250@gmail.com>
2018-09-11 23:19:02 +0100

    Initialise view of application

--- src/main/java/view/MainFrame.java
+++ src/main/java/view/MainFrame.java
@@ -1,10 +1,19 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame {
+    private JTextArea planeStatements;
+    private JPanel planeControlsPanel;
+    private JSlider xAxisControlSlider;
+    private JSlider speedControlSlider;
+
     public MainFrame() {
         super("Simple Airplane Simulator");
 
@@ -14,10 +23,18 @@ public class MainFrame extends JFrame {
     public void initialiseFrame() {
         // Set layout of application
         setLayout(new GridLayout(2, 1));
+        planeControlsPanel = new JPanel(new BorderLayout());
 
         // Create swing components
+        planeStatements = new JTextArea();
+        xAxisControlSlider = new JSlider(JSlider.HORIZONTAL, 0, 10, 5);
+        speedControlSlider = new JSlider(JSlider.VERTICAL, 0, 10, 0);
 
         // Adding the components
+        add(planeStatements);
+        add(planeControlsPanel);
+        planeControlsPanel.add(xAxisControlSlider, BorderLayout.NORTH);
+        planeControlsPanel.add(speedControlSlider, BorderLayout.CENTER);
 
         // Set settings of application
         setVisible(true);
